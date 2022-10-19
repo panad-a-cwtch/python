e6874c1f12add64523766306511d140127237ed0
panad-a-cwtch <stephaniemarie261@gmail.com>
2022-10-19 16:37:16 +0100

    Completed score.py document

--- scores.py
+++ scores.py
@@ -1,2 +1,8 @@
-name = "Steph"
-print(f"Hello {name}")
+english = int(input("Enter your English score: "))
+maths = int(input("Enter your Maths score: "))
+science = int(input("Enter your Science score: "))
+
+average = (english + maths + science)/3
+
+print("Your average score is : ")
+print(average)
