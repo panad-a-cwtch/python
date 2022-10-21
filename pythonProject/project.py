c2c8b3c93c1b8dc855f6585053d049f7cb1d32a3
panad-a-cwtch <stephaniemarie261@gmail.com>
2022-10-21 12:53:53 +0100

    formatting and basic error checks

--- pythonProject/project.py
+++ pythonProject/project.py
@@ -1,4 +1,5 @@
 exit = False
+found = False
 books = {"Miguel de Cervantes" : ["Don Quixote"], "Charles Dickens" : ["A Tale of Two Cities"], "J.R.R Tolkien" : ["The Lord of the Rings", "The Hobbit"], "Antoine de Saint-Exupery" : ["The Little Prince"]}
 
 while not exit:
@@ -23,32 +24,49 @@ while not exit:
         if author in books.keys():
             print(sorted(books[author]))
         else:
-            print(f"{author} is not in our database, sorry.")
+            print(f"\n{author} is not in our database, sorry.")
     
     elif option == 3:
         title = input("Enter the books name: ")
         for key, value in books.items():
             if title in value:
-                print(key)
+                print(f"\nThe author of {title} is {key}.")
+                found = True
 
+        if not found:
+            print(f"\nThe book {title} is not in our system.")
+        
     elif option == 4:
         author = input("Enter the authors name: ")
         title = input("Enter the books name: ")
         books[author] = [title]
+        print(f"\nThe book {title}, by {author} has been added to the system.")
 
     elif option == 5:
         author = str(input("Enter the authors name: "))
         if author in books.keys():
             del books[author]
+            print(f"\n{author} has been removed from our system.")
+            found = True
+        
+        if not found:
+            print(f"\n{author} is not on our system and therefore cannot be removed.")
 
     elif option == 6:
         title = input("Enter the books name: ")
         for value in books.values():
-             if title in value:
+            if title in value:
                 value.remove(title)
-
+                print(f"\n{title} has been removed from our system.")
+                found = True
+        
+        if not found:
+            print(f"\n{title} is not on our system and therefore cannot be removed")
+           
     elif option == 7:
         exit = True
 
     else:
         print("Option not recognised, please try again.")
+
+    found = False
